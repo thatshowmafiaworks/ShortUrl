d887cf19b1c3593c5175eb537248f71c637a9eaf
thatshowmafiaworks <shinehd56@gmail.com>
2025-03-22 23:25:19 +0200

    added unit tests

---
+++ ShortURL.Tests/AboutTextRepositoryTest.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShortUrl.Data;
+using ShortUrl.Models;
+using ShortUrl.Repositories;
+
+namespace ShortURL.Tests
+{
+    public class AboutTextRepositoryTest
+    {
+        private readonly DbContextOptions<ApplicationDbContext> opts;
+
+        public AboutTextRepositoryTest()
+        {
+            opts = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+        }
+        [Fact]
+        public async Task GetLastText_ShouldReturnLastTextByDate()
+        {
+            // Arrange
+            using (var context = new ApplicationDbContext(opts))
+            {
+                var repo = new AboutTextRepository(context);
+
+                var first = new AboutText
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Text = "firstText",
+                    CreatedBy = Guid.NewGuid().ToString(),
+                    Created = DateTime.UtcNow.AddDays(1)
+                };
+                var second = new AboutText
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Text = "secondText",
+                    CreatedBy = Guid.NewGuid().ToString(),
+                    Created = DateTime.UtcNow.AddDays(2)
+                };
+                var third = new AboutText
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Text = "thirdText",
+                    CreatedBy = Guid.NewGuid().ToString(),
+                    Created = DateTime.UtcNow.AddDays(3)
+                };
+
+                context.About.Add(first);
+                context.About.Add(second);
+                context.About.Add(third);
+                await context.SaveChangesAsync();
+
+                // Act
+
+                var result = await repo.GetLastText();
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("thirdText", result.Text);
+            }
+        }
+        [Fact]
+        public async Task UpdateText_ShouldAddNewText()
+        {
+            // Arrange
+            using (var context = new ApplicationDbContext(opts))
+            {
+                var repo = new AboutTextRepository(context);
+
+
+                var old = new AboutText
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Text = "oldText",
+                    CreatedBy = Guid.NewGuid().ToString(),
+                    Created = DateTime.UtcNow
+                };
+                context.About.Add(old);
+                await context.SaveChangesAsync();
+
+                // Act
+                var newText = new AboutText
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Text = "newText",
+                    CreatedBy = Guid.NewGuid().ToString(),
+                    Created = DateTime.UtcNow.AddDays(1)
+                };
+                repo.UpdateText(newText);
+
+                // Assert
+                var result = await repo.GetLastText();
+                Assert.NotNull(result);
+                Assert.Equal("newText", result.Text);
+            }
+        }
+    }
+}
---
+++ ShortURL.Tests/UrlRepositoryTest.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShortUrl.Data;
+using ShortUrl.Models;
+using ShortUrl.Repositories;
+using ShortUrl.Services;
+
+namespace ShortURL.Tests
+{
+    public class UrlRepositoryTest
+    {
+        private readonly IHashGenerator _hashGeneratorMock;
+
+        private readonly DbContextOptions<ApplicationDbContext> dbContextOptions;
+
+        public UrlRepositoryTest()
+        {
+            _hashGeneratorMock = new HashGenerator();
+
+            dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnUrl_WhenUrlExists()
+        {
+            // Arrange
+            using (var context = new ApplicationDbContext(dbContextOptions))
+            {
+                var repo = new UrlRepository(context);
+
+
+                var id = Guid.NewGuid().ToString();
+                var url = "google.com";
+                var hash = _hashGeneratorMock.GenerateHash(url);
+                var expectedUrl = new Url
+                {
+                    Id = id,
+                    UserId = Guid.NewGuid().ToString(),
+                    UrlOriginal = url,
+                    UrlNormalized = url.ToLower(),
+                    UrlShort = $"https://site.com/short/{hash}",
+                    Hash = hash,
+                    Created = DateTime.UtcNow,
+                    Updated = DateTime.UtcNow
+                };
+                context.Urls.Add(expectedUrl);
+                await context.SaveChangesAsync();
+
+                // Act
+                var result = await repo.GetById(id);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal(url, result.UrlOriginal);
+            }
+        }
+
+        [Fact]
+        public async Task GetByHash_ShouldReturnUrl_WhenUrlExists()
+        {
+            // Arrange
+            using (var context = new ApplicationDbContext(dbContextOptions))
+            {
+                var repo = new UrlRepository(context);
+
+                var id = Guid.NewGuid().ToString();
+                var url = "google.com";
+                var hash = _hashGeneratorMock.GenerateHash(url);
+                var expectedUrl = new Url
+                {
+                    Id = id,
+                    UserId = Guid.NewGuid().ToString(),
+                    UrlOriginal = url,
+                    UrlNormalized = url.ToLower(),
+                    UrlShort = $"https://site.com/short/{hash}",
+                    Hash = hash,
+                    Created = DateTime.UtcNow,
+                    Updated = DateTime.UtcNow
+                };
+                context.Urls.Add(expectedUrl);
+                await context.SaveChangesAsync();
+
+                // Act
+
+                var result = await repo.GetByHash(hash);
+
+                // Assert
+
+                Assert.NotNull(result);
+                Assert.Equal(hash, result.Hash);
+            }
+
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnAllUrls_WhenUrlsExists()
+        {
+            // Arrange
+            using (var context = new ApplicationDbContext(dbContextOptions))
+            {
+                var repo = new UrlRepository(context);
+
+
+                var id = Guid.NewGuid().ToString();
+                var url = "google.com";
+                var hash = _hashGeneratorMock.GenerateHash(url);
+                var expectedUrl1 = new Url
+                {
+                    Id = id,
+                    UserId = Guid.NewGuid().ToString(),
+                    UrlOriginal = url,
+                    UrlNormalized = url.ToLower(),
+                    UrlShort = $"https://site.com/short/{hash}",
+                    Hash = hash,
+                    Created = DateTime.UtcNow,
+                    Updated = DateTime.UtcNow
+                };
+                var id2 = Guid.NewGuid().ToString();
+                var url2 = "google.com";
+                var hash2 = _hashGeneratorMock.GenerateHash(url);
+                var expectedUrl2 = new Url
+                {
+                    Id = id2,
+                    UserId = Guid.NewGuid().ToString(),
+                    UrlOriginal = url2,
+                    UrlNormalized = url2.ToLower(),
+                    UrlShort = $"https://site.com/short/{hash2}",
+                    Hash = hash2,
+                    Created = DateTime.UtcNow,
+                    Updated = DateTime.UtcNow
+                };
+                context.Urls.Add(expectedUrl1);
+                context.Urls.Add(expectedUrl2);
+                await context.SaveChangesAsync();
+
+                // Act
+                var result = await repo.GetAll();
+
+                // Assert
+                Assert.NotNull(result);
+                foreach (var item in result)
+                {
+                    Assert.NotNull(item);
+                }
+                Assert.Equal(url, result.First().UrlOriginal);
+                Assert.Equal(url2, result.Last().UrlOriginal);
+            }
+        }
+
+        [Fact]
+        public async Task Add_ShouldCreateNewUrl()
+        {
+            // Arrange
+            using (var context = new ApplicationDbContext(dbContextOptions))
+            {
+                var repo = new UrlRepository(context);
+
+                var id = Guid.NewGuid().ToString();
+                var url = "google.com";
+                var hash = _hashGeneratorMock.GenerateHash(url);
+                var newUrl = new Url
+                {
+                    Id = id,
+                    UserId = Guid.NewGuid().ToString(),
+                    UrlOriginal = url,
+                    UrlNormalized = url.ToLower(),
+                    UrlShort = $"https://site.com/short/{hash}",
+                    Hash = hash,
+                    Created = DateTime.UtcNow,
+                    Updated = DateTime.UtcNow
+                };
+
+                // Act
+
+                await repo.Add(newUrl);
+
+                // Assert
+                var result = await context.Urls.FirstOrDefaultAsync(x => x.UrlOriginal == url);
+                Assert.NotNull(result);
+                Assert.Equal(newUrl.UrlOriginal, result.UrlOriginal);
+            }
+        }
+
+        [Fact]
+        public async Task Delete_ShouldDeleteExisting()
+        {
+            // Arrange
+            using (var context = new ApplicationDbContext(dbContextOptions))
+            {
+                var repo = new UrlRepository(context);
+
+                var id = Guid.NewGuid().ToString();
+                var url = "google.com";
+                var hash = _hashGeneratorMock.GenerateHash(url);
+                var existingUrl = new Url
+                {
+                    Id = id,
+                    UserId = Guid.NewGuid().ToString(),
+                    UrlOriginal = url,
+                    UrlNormalized = url.ToLower(),
+                    UrlShort = $"https://site.com/short/{hash}",
+                    Hash = hash,
+                    Created = DateTime.UtcNow,
+                    Updated = DateTime.UtcNow
+                };
+                context.Urls.Add(existingUrl);
+                await context.SaveChangesAsync();
+
+                // Act
+
+                var toDelete = await repo.GetById(id);
+                repo.Delete(toDelete);
+
+                // Assert
+                var result = await context.Urls.FirstOrDefaultAsync(x => x.Id == id);
+                Assert.Null(result);
+            }
+        }
+    }
+}
